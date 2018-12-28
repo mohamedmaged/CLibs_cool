67802b05b0da7006efaf92066f7a174662e2f5a1
mohamed maged <mohamedmaged35@yahoo.com>
2018-12-28 17:31:02 +0200

    added get value of a slot

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import net.sf.clipsrules.jni.Environment;
+import net.sf.clipsrules.jni.FactAddressValue;
+import net.sf.clipsrules.jni.MultifieldValue;
 import net.sf.clipsrules.jni.PrimitiveValue;
 
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Environment clips = new Environment();
         clips.loadFromResource("/cool.clp");
@@ -24,5 +26,10 @@ public class Main {
         PrimitiveValue value=clips.eval("(facts)");
         String ou = value.toString();
         System.out.println(ou);
+
+        FactAddressValue fv = (FactAddressValue)((MultifieldValue) clips.eval("(find-fact ((?x Coolness)) TRUE)")).get(0);
+        String m = fv.getFactSlot("cool").toString();
+        System.out.println(m);
+
     }
 }
